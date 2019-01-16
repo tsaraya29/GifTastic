789765ae01c58b1aa2fba02381d1d24bfe69fcb0
T Saraya <saratgrffn@gmail.com>
2019-01-15 23:58:16 -0600

    work so far; need to 1 - configure API to work with search and at least 10 items, make added buttons work as well, animate gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,19 @@
 
-// //figuring out how to dynamically create button
 
-// var sampleButton = $("<button>")
-// sampleButton.text("test");
-// $("#buttons-go-here").append(sampleButton);
+var topics = ["oceans", "rain","thunder","rainbows","mountains","fields","rivers","clouds" ];
 
-
-
-var topics = ["oceans", "rain","thunder","rainbows","wells","fields","rivers","clouds" ];
-
+//Loop to create buttons from array
 
 for (var i = 0; i<topics.length; i++) {
     
     var topicButton = $("<button>");
+    topicButton.addClass("themes")
 
     topicButton.text(topics[i]);
   
     $("#buttons-go-here").append(topicButton);
 }
-
-
-// // Creating new buttons from topic
+// Button to create more buttons
 $("#topicAdd").on("click", function(event) {
 
     event.preventDefault();
@@ -29,8 +22,43 @@ $("#topicAdd").on("click", function(event) {
 
 //     // Get the to-do "value" from the textbox and store it a variable
     var newTopic = $("#term").val().trim(); 
-    var newButton = $("<button>").text(newTopic)
+    var newButton = $("<button>").text(newTopic);
+    newButton.addClass("themes");
     $("#buttons-go-here").append(newButton);
 
 });
+
+// // Get Giphy with it
+$('.themes').on("click", function() {
+    event.preventDefault();
+    // Storing our giphy API URL for a random images based on button clicked
+
+    var buttonChoice = $(this).text();
+        console.log(buttonChoice);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=U8JI8rVdSJlQ8F9WXg9VSbn8W0Sls1zK&tag=" + buttonChoice +"";
+     // Perfoming an AJAX GET request to our queryURL
+     $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      // After the data from the AJAX request comes back
+      .then(function(response) {
+
+        // Saving the image_original_url property
+          var imageUrl = response.data.image_original_url;
+
+          // Creating and storing an image tag
+          var catImage = $("<img>");
+
+          // Setting the catImage src attribute to imageUrl
+          catImage.attr("src", imageUrl);
+          catImage.attr("alt", "cat image");
+           // Prepending the catImage to the images div
+           $("#gifs-go-here").prepend(catImage);
+        });
+    });
+
+
+
 
